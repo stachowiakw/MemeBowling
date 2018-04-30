63c766e4c628890208c9c466b738c3f95a24b1b0
Wojtek-PC\wojciech.stachowiak <stachowiakw@gmail.com>
2018-04-30 23:02:10 +0200

    Test 1

--- MemeBowling/Assets/ActionMaster.cs
+++ MemeBowling/Assets/ActionMaster.cs
@@ -3,14 +3,40 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ActionMaster : MonoBehaviour {
+    //private int[] bowls = new int[21];
+    private int bowl = 1;
 
     public enum Action {Tidy, Reset, EndTurn, EndGame};
+    
 
     public Action Bowl (int pins)
     {
-        if (pins < 0 || pins > 10) { throw new UnityException("Invalid Pins")}
+        if (pins == 11)
+        {
+            return Action.Reset;
+        }
+
+        if (pins < 0 || pins > 10)
+        {
+            throw new UnityException("Invalid Pins");
+        }
+
         if (pins == 10)
-        { return Action.EndTurn; }
+        {
+            bowl += 2;
+            return Action.EndTurn;
+        }
+
+        if (bowl % 2 != 0)
+        {
+            bowl += 1;
+            return Action.Tidy;
+        }
+        else if (bowl % 2 == 0)
+        {
+            bowl += 1;
+            return Action.EndTurn;
+        }
 
         // other behaviour here
         throw new UnityException("Not sure what action to return!");
--- MemeBowling/Assets/Editor/ActionMasterTest.cs
+++ MemeBowling/Assets/Editor/ActionMasterTest.cs
@@ -6,7 +6,17 @@ using UnityEngine;
 [TestFixture]
 public class ActionMasterTest
 {
+    private ActionMaster actionMaster;
     private ActionMaster.Action endTurn = ActionMaster.Action.EndTurn;
+    private ActionMaster.Action tidy = ActionMaster.Action.Tidy;
+    private ActionMaster.Action reset = ActionMaster.Action.Reset;
+
+
+    [SetUp]
+    public void Setup()
+    {
+        actionMaster = new ActionMaster();
+    }
 
     [Test]
     public void PassingTest()
@@ -17,7 +27,25 @@ public class ActionMasterTest
     [Test]
     public void T01OneStrikeReturnsEndTurn()
     {
-        ActionMaster actionMaster = new ActionMaster();
         Assert.AreEqual (endTurn,actionMaster.Bowl(10));
+    }
+
+    [Test]
+    public void T02Bowl8ReturnsTidy()
+    {
+        Assert.AreEqual(tidy, actionMaster.Bowl(8));
+    }
+
+    [Test]
+    public void T03EloEloMakrelo()
+    {
+        Assert.AreEqual(reset, actionMaster.Bowl(11));
+    }
+
+    [Test]
+    public void T04Bowl28SpareReturnsEndTurn()
+    {
+        actionMaster.Bowl(8);
+        Assert.AreEqual(endTurn, actionMaster.Bowl(2));
     }
 }
