2b1b3583f7f32ce1ef86a3fd2757be42931db63f
GamingCenter7\Wojciech Stachowiak <stachowiakw@gmail.com>
2018-04-30 16:55:39 +0200

    Last Commit GC7

---
+++ MemeBowling/Assets/ActionMaster.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActionMaster : MonoBehaviour {
+
+    public enum Action {Tidy, Reset, EndTurn, EndGame};
+
+    public Action Bowl (int pins)
+    {
+        if (pins < 0 || pins > 10) { throw new UnityException("Invalid Pins")}
+        if (pins == 10)
+        { return Action.EndTurn; }
+
+        // other behaviour here
+        throw new UnityException("Not sure what action to return!");
+    }
+}
