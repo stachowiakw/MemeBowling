9208849eee41eb2852643aa94aee4019e7c94347
Wojciech Stachowiak <stachowiakw@gmail.com>
2018-09-12 23:59:33 +0200

    L249

--- MemeBowling/Assets/Editor/ScoreMasterTest.cs
+++ MemeBowling/Assets/Editor/ScoreMasterTest.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using UnityEngine;
 
@@ -11,5 +12,37 @@ public class ScoreTest
     public void PassingTest()
     {
         Assert.AreEqual(1, 1);
+    }
+
+    [Test]
+    public void T02Bowl12()
+    {
+        int[] rolls = { 1, 2 };
+        int[] frames = { 3 };
+        Assert.AreEqual(frames.ToList(), ScoreMaster.ScoreFrames (rolls.ToList()));
+    }
+
+    [Test]
+    public void T02Bowl1234567890()
+    {
+        int[] rolls = { 1, 2, 3, 4, 5, 4, 7, 1, };
+        int[] frames = { 3, 7, 9, 8 };
+        Assert.AreEqual(frames.ToList(), ScoreMaster.ScoreFrames(rolls.ToList()));
+    }
+
+    [Test]
+    public void T03StrikeFirstBowl()
+    {
+        int[] rolls = { 10, 2, 3 };
+        int[] frames = { 15, 5 };
+        Assert.AreEqual(frames.ToList(), ScoreMaster.ScoreFrames(rolls.ToList()));
+    }
+
+    [Test]
+    public void T04SpareFirstBowl()
+    {
+        int[] rolls = { 1, 9, 2, 3 };
+        int[] frames = { 12, 5 };
+        Assert.AreEqual(frames.ToList(), ScoreMaster.ScoreFrames(rolls.ToList()));
     }
 }
--- MemeBowling/Assets/Scripts/ScoreMaster.cs
+++ MemeBowling/Assets/Scripts/ScoreMaster.cs
@@ -3,8 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class ScoreMaster {
-    
-    //return  a list of cumulative scores, like a normal score card.
+    // roll = it is a score achieved in a single bowl.
+    // rolls = roll many times.
+
+    //return a list of cumulative scores, like a normal score card.
     public static List<int> ScoreCumulative(List<int> rolls)
     {
         List<int> cumulativesScores = new List<int>();
@@ -21,10 +23,22 @@ public class ScoreMaster {
     // return a list of individual frame scores, not cummulative
     public static List<int> ScoreFrames (List<int> rolls)
     {
-        List<int> frameList = new List<int>();
+        List<int> frames= new List<int>();
 
-        // your code
+        for (int i = 1; i < rolls.Count; i += 2)
+        {
+            if(frames.Count == 10 ) { break; }                      // prevent goint to the 11th frame which does not exist
+                
+            if (rolls[i - 1] +rolls[i] < 10)                        // normal "open" frame
+            { frames.Add(rolls[i - 1] + rolls[i]); }
 
-        return frameList;
+            if (rolls.Count - 1 <= 1) { break; }                    // Insufficient look-ahead
+
+            if (rolls[i - 1] == 10)                                 // STRIKE
+            { i--; frames.Add(10 + rolls[i + 1] + rolls[i + 2]);
+    }       else if (rolls[i - 1] + rolls[i] == 10)                 // calculate SPARE bonus
+            { frames.Add(10 + rolls[i + 1]); }
+        }
+        return frames;
     }
 }
